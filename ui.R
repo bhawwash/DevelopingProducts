ee67f33f727c6cdcfa3c5dd18813415d90dd958f
bhawwash <bhawwash@gmail.com>
2015-05-19 16:14:42 -0400

    updates

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(width=3,
       h4("Description"),
-      p("This app shows the results of using K-Means to cluster Iris Dataset. You can change the number of clusters and/or the type of algorithm. Moreover, you can select which features to show. You can compare the performance using the vlidation metrics"),
+      p("This app shows the results of using K-Means to cluster mtcars Dataset. You can change the number of clusters and/or the type of algorithm. Moreover, you can select which features to show. You can compare the performance using the vlidation metrics"),
       sliderInput("k",label=h4("Number of Clusters:"),
                   min = 2,max = 10,value = 5),
       radioButtons("alg", label = h4("Algorithm Used"),
